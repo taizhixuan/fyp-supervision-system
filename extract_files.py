832204a1a7baf8002c37866f3cff7609ad236bde
Httpsouls <taizhixuan@gmail.com>
2026-06-21 05:29:37 +0800

    update

--- extract_files.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('docs-project/knowledge-graph.json') as f:
-    kg = json.load(f)
-
-all_files = sorted(kg['knowledge_graph'].keys())
-print(f"Total files in knowledge graph: {len(all_files)}")
-for f in all_files:
-    print(f)
